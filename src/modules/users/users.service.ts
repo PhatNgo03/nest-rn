bbf3acb185baf8712cee612e146f608c57253dea
PhatNgo03 <phatngo1908@gmail.com>
2024-10-09 21:51:36 +0700

    fix bugs

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -60,7 +60,15 @@ export class UsersService {
       .select("-password")
       .sort(sort as any)
 
-    return { results, totalPages };
+    return {
+      meta: {
+        current: current,
+        pageSize: pageSize,
+        pages: totalPages,
+        total: totalItems
+      },
+      results
+    };
   }
 
   findOne(id: number) {
